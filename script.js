2abb1b63423860b80bf2aa110e4a44b2f69f5de5
Lucrecia Aloi <lumad29@gmail.com>
2023-09-20 21:50:27 -0300

    Proyecto actualizado pre-entrega-2

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-let message =
-  "pick your product\n1- Lawn Mower - $250 \n2- Garden Tool Set $20\n3- Pack of seeds $10 \n0- Exit";
-let option;
-let total = 0;
+// array de objetos
+let products = [
+  { id: 13, name: "Lawn Mower", price: 250, stock: 3 },
+  { id: 42, name: "Garden Tool Set", price: 25, stock: 8 },
+  { id: 63, name: "Seeds", price: 10, stock: 10 },
+  { id: 96, name: "Plants", price: 12, stock: 9 },
+];
 
-const LawnMower = 250;
-const gardenToolSet = 20;
-const seeds = 10;
+let message = "Pick an option:\n1 - show products\n2 - Add to cart\n3 - Finish shopping\n0 - Quit";
 
-let shop = function () {
+shop([products]); // shp()
+
+function shop(products) {
+  let cart = [];
+  let option;
+
   do {
     option = Number(prompt(message));
     switch (option) {
       case 1:
-        alert("Lawn Mower added to cart");
-        total += LawnMower;
-        alert("your total is $" + total);
+        alert(showProducts());
         break;
       case 2:
-        alert("Garden Tool Set added to cart");
-        total += gardenToolSet;
-        alert("your total is $" + total);
+        addToCart(products, cart);
         break;
       case 3:
-        alert("Pack of seeds added to cart");
-        total += 10;
-        alert("your total is $" + total);
+        finishShopping(cart);
+        cart = [];
+        break;
+      default:
         break;
     }
   } while (option != 0);
 
   if (option === 0) {
-    // alert("are you sure you want to exit?");
     let answer = prompt("are you sure you want to exit?\n1- Yes\n2- No");
     if (Number(answer) === 1) {
       alert("Thank you for shopping");
@@ -38,6 +40,47 @@ let shop = function () {
       shop();
     }
   }
-};
+}
 
-shop();
+// show products function
+function showProducts() {
+  return products.map(product => product.id + " - " + product.name).join("\n");
+}
+
+// sadd to cart function
+function addToCart(allProducts, cart) {
+  let id = Number(prompt("Select product by id:\n" + showProducts(allProducts)));
+  let productFound = products.find(product => product.id === id);
+
+  let addedToCart = cart.find(product => product.id === productFound.id);
+
+  if (productFound.stock > 0) {
+    if (addedToCart) {
+      addedToCart.quantity++;
+      addedToCart.subtotal = addedToCart.quantity * addedToCart.price;
+    } else {
+      cart.push({
+        id: productFound.id,
+        name: productFound.name,
+        price: productFound.price,
+        quantity: 1,
+        subtotal: productFound.price,
+      });
+    }
+    productFound.stock--;
+    alert("product added to cart");
+  } else {
+    alert("product not in stock");
+  }
+}
+
+// finish shopping
+function finishShopping(cart) {
+  if (cart.length === 0) {
+    alert("you should add the product to cart first!");
+  } else {
+    let total = cart.reduce((acum, product) => acum + product.subtotal, 0);
+    alert("Your total is: " + total);
+    alert("Thank you for shopping");
+  }
+}
